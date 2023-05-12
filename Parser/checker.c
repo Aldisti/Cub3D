777b68d2b9e6d5b6dadf4cddd5208dc936539f8f
Aldisti <alessandrodiste99@gmail.com>
2023-05-12 17:00:30 +0200

    Automated push by Aldisti

--- Parser/checker.c
+++ Parser/checker.c
@@ -6,7 +6,7 @@
 /*   By: adi-stef <adi-stef@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/12 16:20:49 by adi-stef          #+#    #+#             */
-/*   Updated: 2023/05/12 16:36:36 by adi-stef         ###   ########.fr       */
+/*   Updated: 2023/05/12 16:59:48 by adi-stef         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -40,7 +40,8 @@ int	ft_check_border(t_pars *pars)
 			return (1);
 	i = -1;
 	while (pars->mat[pars->height - 1][++i])
-		if (pars->mat[pars->height - 1][i] != 49 && pars->mat[pars->height - 1][i] != 32)
+		if (pars->mat[pars->height - 1][i] != 49
+			&& pars->mat[pars->height - 1][i] != 32)
 			return (1);
 	j = -1;
 	while (pars->mat[++j])
@@ -59,9 +60,49 @@ int	ft_check_border(t_pars *pars)
 	return (0);
 }
 
+/*
+DESCRIPTION
+this function will will make sure that for every space in s1 there is a wall in
+s2 and viceversa
+*/
+int	ft_space_cmp(const char *s1, const char *s2)
+{
+	int	i;
+	int	j;
+	
+	i = 0;
+	while (s1[i] && s2[i])
+	{
+		if (s1[i] == 32 && s2[i] != 49 && s2[i + 1] != 49 && s2[i + 1]
+			&& (i == 0 || s2[i - 1] != 49))
+			return (0);
+	}
+}
+
+int	ft_check_space(t_pars *pars)
+{
+	int	i;
+	int	j;
+
+	j = -1;
+	while (pars->mat[++j])
+	{
+		if (j == 0)
+		{
+			if (ft_strlen(pars->mat[j]) > ft_strlen(pars->mat[j + 1]))
+			{
+				
+			}
+		}
+	}
+	return (0);
+}
+
 int	ft_check_mat(t_pars *pars)
 {
 	if (ft_check_border(pars))
+		return (1);
+	if (ft_check_space(pars))
 		return (1);
 	return (0);
 }
